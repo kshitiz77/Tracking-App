4abf5b068bed6213224539826fb3cb3e67cef522
Kshitiz Sharma <kshitiz.sharma@velocis.in>
2022-05-25 18:12:25 +0530

    fetch latitude and longitude data

--- App.js
+++ App.js
@@ -1,36 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, {useState, useEffect} from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import Geolocation from '@react-native-community/geolocation';
 
 const App = () => {
   const [currentLocation, setCurrentLocation] = useState({
-    latitude: "",
-    longitude: ""
-  })
-const {longitude, latitude} = currentLocation;
-  useEffect(()=>{
-    Geolocation.getCurrentPosition(info => setCurrentLocation({latitude:info.coords.latitude, longitude:info.coords.longitude}));
-    Geolocation.watchPosition(info => console.log(info))
+    latitude: '',
+    longitude: '',
+  });
+
+  const {longitude, latitude} = currentLocation;
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) =>{
+      setCurrentLocation({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+      });
+      // console.log(info, "Current Position")
     })
+  },[currentLocation]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>latitude: {latitude}</Text>
       <Text style={styles.titleStyle}>longitude: {longitude}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  titleStyle:{
-    fontSize:30,
-    fontWeight:'bold',
-    marginVertical:20
-  }
-})
+  titleStyle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+});
 
-export default App
+export default App;
